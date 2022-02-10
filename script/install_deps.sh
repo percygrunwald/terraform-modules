3153d453415e36283140881f52df05ffb7542277
Percy Grunwald <percy.grunwald@gmail.com>
2022-02-09 21:39:58 -0700

    Make tflint an asdf dependency

--- script/install_deps.sh
+++ script/install_deps.sh
@@ -14,13 +14,6 @@ done <requirements-golang.txt
 printf "Installing global pip dependencies...\n"
 pip install -r requirements-python.txt
 
-printf "Installing tflint...\n"
-if which tflint > /dev/null; then
-  echo "tflint already installed."
-else
-  curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
-fi
-
 if which asdf > /dev/null; then
   printf "Ensuring asdf shims are up to date...\n"
   asdf reshim golang
